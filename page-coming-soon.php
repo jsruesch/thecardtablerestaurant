7ecbdd9c7cf566470cccb2ef75642d5514bbb7e2
jsruesch <jake@gofarwell.com>
2019-11-06 20:07:23 -0600

    responsive fixes

--- page-coming-soon.php
+++ page-coming-soon.php
@@ -20,11 +20,11 @@
                   <hr class="mar20-bot">
                   <div class="full">
                      <p class="text-white">We're excited to announce that <span class="text-bold text-gold">The Card Table</span> is set to open winter 2019 located inside the Sun Prairie Ice Arena at 
-                     <a class="text-gold" href="https://goo.gl/maps/n8rQqxDiTbxPFAaX6" target="_blank">836 Grove St, Sun Prairie, WI 53590</a>! <span class="text-bold text-gold">The Card Table</span> will be  <span class="text-bold text-gold">The Card Table</span> will feature a family friendly environment where everyone can feel welcome. Stay Tuned!</p>
+                     <a class="text-gold" href="https://goo.gl/maps/n8rQqxDiTbxPFAaX6" target="_blank">836 Grove St, Sun Prairie, WI 53590</a>! <span class="text-bold text-gold">The Card Table</span> will feature a family friendly environment where everyone can feel welcome. Stay Tuned!</p>
                   </div>
                   <hr class="mar20-top mar20-bot">
                   <div class="full">
-                     <a href="mailto:thecardtablerestaurant@gmail.com" target="_blank" class="full redbox">For more information contact <span class="text-gold">thecardtablerestaurant@gmail.com</span></a>
+                     <a href="mailto:thecardtablerestaurant@gmail.com" target="_blank" class="full redbox">For more information contact <span class="text-gold">here</span></a>
                   </div>
                   
                </div>
